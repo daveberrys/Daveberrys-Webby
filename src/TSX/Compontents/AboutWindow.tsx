0d5472131235a11ee4fa5f232787e6f3f364912d
daveberrys <84567826+daveberrys@users.noreply.github.com>
2025-12-27 23:56:58 +0800

    is this brocen

--- src/TSX/Compontents/AboutWindow.tsx
+++ src/TSX/Compontents/AboutWindow.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Window from './Window';
+import React from "react";
+import Window from "./Handlers/Window";
 
 interface AboutWindowProps {
   onClose: () => void;
@@ -10,18 +10,28 @@ const AboutWindow: React.FC<AboutWindowProps> = ({ onClose }) => {
     <Window title="About me!" onClose={onClose}>
       <ul>
         <b> Main infomation: </b>
-        <li> Age is private, but a teenager. </li>
         <li> Australian and Malaysian. </li>
         <li> Programmer, Hardware, Voice Actor, Artist and Designer. </li>
-        
-        <br/>
-        
+
+        <br />
+
         <b> Other infomation: </b>
-        <li> Web developer, software developer, frontend developer, (somewhat) backend developer. </li>
+        <li>
+          Web developer, software developer, frontend developer, (somewhat)
+          backend developer.
+        </li>
         <li> Software and hardware developer for random hobby projects. </li>
-        <li> I want to make a team that's dedicated to make great and open source software. </li>
-        <li> I <i>do</i> use AI, but I check code before full sending it. </li>
-        <li> I use <a href="https://zed.dev">Zed</a> instead of <a href="https://vscode.dev">Visual Studio Code</a>. </li>
+        <li>
+          I want to make a team that's dedicated to make great and open source
+          software.
+        </li>
+        <li>
+          I <i>do</i> use AI, but I check code before full sending it.{" "}
+        </li>
+        <li>
+          I use <a href="https://zed.dev">Zed</a> instead of{" "}
+          <a href="https://vscode.dev">Visual Studio Code</a>.{" "}
+        </li>
       </ul>
     </Window>
   );
